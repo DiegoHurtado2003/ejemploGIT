1e79b89e864739caac9be7f26821471204a375ac
DiegoHurtado2003 <diego.hurtado@iesnervion.es>
2021-11-09 12:09:16 +0100

    Local Changes 2

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hola bebe");
-        System.out.println("Ya que contigo no sirve la labia");
+        System.out.println("Ya que contigo no sirve la labia y te crees muy sabia");
     }
 }
